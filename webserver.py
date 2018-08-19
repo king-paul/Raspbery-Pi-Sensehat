f2a64d7b844f933940ef407683bd9bad59a3f8a1
Paul <s3449513@student.rmit.edu.au>
2018-08-19 15:45:14 +1000

    Added accelerometer and oriention data to webpage

--- webserver.py
+++ webserver.py
@@ -34,17 +34,67 @@ def getTempAndHumid():
 			'date_time': dateTimes[row],
 			'temperature': temps[row],
 			'humidity': humids[row]
-		}
-		)
+		})
+		
+	return jsonData
+
+def getAccelAndOrient():
+
+# create empty json objecy
+	jsonData = [{}]
+
+	# Date and time of logging
+	cursor.execute("SELECT datetime(date_time, 'localtime') FROM accel_and_orient")
+	dateTimes = cursor.fetchall()
+
+	'''Acceleromater'''
+	# X Acceleration
+	cursor.execute("SELECT round(x, 2) FROM accel_and_orient")
+	accelX = cursor.fetchall()
+
+	# Y Acceleration
+	cursor.execute("SELECT round(y, 2) FROM accel_and_orient")
+	accelY = cursor.fetchall()
+
+	# Z
+	cursor.execute("SELECT round(z, 2) FROM accel_and_orient")
+	accelZ = cursor.fetchall()
+
+	'''Orientation'''
+	# Pitch
+	cursor.execute("SELECT round(pitch, 2) FROM accel_and_orient")
+	pitches = cursor.fetchall()
+
+	# Roll
+	cursor.execute("SELECT round(roll, 2) FROM accel_and_orient")
+	rolls = cursor.fetchall()
+
+	# Yaw
+	cursor.execute("SELECT round(yaw, 2) FROM accel_and_orient")
+	yaws = cursor.fetchall()
+	
+	# iterate through the table adding each row to the json object
+	for row in range(len(dateTimes)):
+		jsonData.append({
+			'date_time': dateTimes[row],
+			'x': accelX[row],
+			'y': accelY[row],
+			'z': accelZ[row],
+			'pitch': pitches[row],
+			'roll': rolls[row],
+			'yaw': yaws[row]
+		})
 		
 	return jsonData
 
 # main route 
 @app.route("/")
 def index():	
-	data = getTempAndHumid()
+	temp_humid_data = getTempAndHumid()
+	accel_orient_data = getAccelAndOrient()
 	
-	return render_template('index.html',  rows=data)
+	return render_template('index.html',  tempTable = temp_humid_data,
+	accelTable = accel_orient_data)
 
 # starts the web server
 if __name__ == "__main__":	
